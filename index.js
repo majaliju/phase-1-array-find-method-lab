110596fe878263ba3e379c386db54527ac9fbca6
majaliju <majalijunyc@gmail.com>
2022-01-31 11:32:13 -0500

    updated

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// code your solution here
+// passes first test but not second test
+// in 2 parts
+function superbowlWin(records){
+    debugger;
+    const winResult = records.find(winningYear);
+    debugger;
+    return winResult.year
+    debugger;
+}
+
+const winningYear = function(record) {
+    return record.result === 'W'  
+}
+
+
+// passes first test but not second test
+// all in one function
+/*
+function superbowlWin(records){
+    debugger;
+    const winResult = records.find(function(record) {
+        debugger;
+        return record.result === 'W'  
+        debugger;
+    })
+    return winResult.year;
+    debugger;
+}
+*/
+
+
+// written with an if, else condition ; in 2 parts
+// passes second test but not first
+/*
+function superbowlWin(records){
+    const winResult = records.find(winningYear);
+    if (winResult === true){
+        return winResult.year
+    }
+    else if (winResult === false){
+        return undefined;
+    }
+    
+}
+
+const winningYear = function(record) {
+    return record.result === 'W'  
+}
+*/
+
+
+
